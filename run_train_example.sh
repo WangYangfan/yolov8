1ae2b577feaa5243ee16947e33246220359a74f7
WangYangfan <18232948487@163.com>
2024-06-02 14:32:23 +0800

    Git LFS commit

--- run_train_example.sh
+++ run_train_example.sh
@@ -1,1 +1,3 @@
-yolo task=detect mode=train model=yolov8n.pt data=mydata.yml batch=32 epochs=10 imgsz=640 workers=10 device=0
+version https://git-lfs.github.com/spec/v1
+oid sha256:de277bd4c9730c2ee7c06bb043ab7db8bfb073174bb7e03327255f0e39db26ea
+size 110
--- run_valid_example.sh
+++ run_valid_example.sh
@@ -1,1 +1,3 @@
-yolo task=detect mode=val model=runs/detect/train2/weights/best.pt data=mydata.yml device=0
+version https://git-lfs.github.com/spec/v1
+oid sha256:2767acbe37bce299dbc6e627fae9bad5e4296b02d4db243d34e53fd480d4bc0f
+size 92
