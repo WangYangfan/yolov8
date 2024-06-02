60d9ceeebfbe5b2a46228c9845e7032c8b970301
Wayfarer <32709746+WangYangfan@users.noreply.github.com>
2024-06-02 14:44:25 +0800

    Update run_valid_example.sh

--- run_valid_example.sh
+++ run_valid_example.sh
@@ -1,3 +1,1 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2767acbe37bce299dbc6e627fae9bad5e4296b02d4db243d34e53fd480d4bc0f
-size 92
+yolo task=detect mode=val model=runs/detect/train2/weights/best.pt data=mydata.yml device=0
